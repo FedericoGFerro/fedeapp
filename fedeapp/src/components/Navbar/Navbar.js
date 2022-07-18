9e6d79c73e2f64017741eeb49a9ab39298b3d57d
Fede Ferro <federicogonzaloferro@gmail.com>
2022-07-18 20:34:07 -0300

    Navbar

---
+++ fedeapp/src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    returno (
+        <nav className='Navbar'>
+            <div>
+                Tienda
+            </div>
+            <div>
+                <button>Categorias</button>
+                <button>Ofertas</button>
+                <button>Informacion</button>
+                <button>Promociones</button>
+                <button>Contacto</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar

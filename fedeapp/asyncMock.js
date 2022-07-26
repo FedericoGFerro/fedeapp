ebc6500bdd2caab2626a288965f72b8cd0645ba1
Fede Ferro <federicogonzaloferro@gmail.com>
2022-07-25 21:50:36 -0300

    Entrega 4

---
+++ fedeapp/asyncMock.js
@@ -0,0 +1,13 @@
+const products = [
+    { 
+        id: '1', 
+        name: 'Pizza', 
+        price: 100, 
+        category: 'Pizza', 
+        img:'https://placeimg.com/640/480/any', 
+        stock: 25, 
+        description:'Napolitana'
+    },
+    { id: '2', name: 'Hamburguesa', price: 150, category: 'hamburguesa', img:'https://placeimg.com/640/480/any', stock: 16, description:'Hamburguesa'},
+    { id: '3', name: 'Ensalada', price: 120, category: 'ensalada', img:'https://placeimg.com/640/480/any', stock: 10, description:'Ensalada'}
+]
--- fedeapp/src/components/ItemListContainer/ItemListContainer.js
+++ fedeapp/src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,20 @@
+import './ItemListContainer.css';
+import Counter from '../ItemCount/ItemCount';
+
 const ItemListContainer = (props) => {
+
+    const handleOnAdd = (quantity) => {
+        alert(`Agregaste ${quantity} productos`);
+    }
     return (
-        <h1>{props.greeting}</h1>
-    )
+        <div>
+            <h1 className="item_greeting">{props.greeting}</h1>
+            <div className='card_container'>
+                <img className='card_img' src='' alt=''/>
+                <Counter initial={1} stock={10} onAdd={handleOnAdd}/>
+            </div>
+        </div>
+    );
 }
 
-export default ItemListContainer
+export default ItemListContainer;
